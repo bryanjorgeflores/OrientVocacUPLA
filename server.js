39dc0c1f84557922bbbdffe70a18d80a682e65ea
bryanjorgeflores <bryanjorgeflores@gmail.com>
2019-04-10 17:38:40 -0500

    cambios

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const express = require('express');  
-const app = express();  
-app.use(express.static(__dirname + '/dist'));  
-app.all('*', (req, res) => {  
-  res.status(200).sendFile(__dirname + '/dist/index.html');  
-});  
-app.listen(process.env.PORT || 8080); 
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/testVocacional'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/testVocacional/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
