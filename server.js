38bf288ac85caaa213f415450d453d6bd0034166
bryanjorgeflores <bryanjorgeflores@gmail.com>
2019-04-17 12:55:29 -0500

    Actualizacion

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/testVocacional'));
 
-app.get('/orientacionvocacional', function(req,res) {
+app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname,'/dist/testVocacional/index.html'));
 });
