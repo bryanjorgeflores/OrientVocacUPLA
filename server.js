d112f67ec3885d6945f037fe6c9a1c6b33e4c8b8
bryanjorgeflores <bryanjorgeflores@gmail.com>
2019-04-17 12:39:45 -0500

    Actualizacion de server

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/testVocacional'));
 
-app.get('/*', function(req,res) {
+app.get('/orientacionvocacional', function(req,res) {
     
 res.sendFile(path.join(__dirname,'/dist/testVocacional/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);c
